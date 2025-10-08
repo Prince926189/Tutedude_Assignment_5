bda9d424978fafbb8a7f01af9a3acc683b2dc537
Prince926189 <panditprince88435@gmail.com>
2025-10-08 16:02:06 +0530

    Initial commit

--- task_1.py
+++ task_1.py
@@ -1,21 +1,3 @@
-'''
-clas = {
-    "ritik" : 90,
-    "prince" : 99,
-    "leena" : 89,
-    "sakshit" :87,
-    "ram" : 100
-}
-y = True
-x = input("Enter the student name : ")
-for i in clas:
-    if i == x:
-        print(f"{x}'s marks : {clas[i]}")
-        y = False
-if y == True:
-    print("Student not found ,try again")
-'''
-
 students = {
     "ritik" : 90,
     "prince" : 99,
@@ -25,13 +7,10 @@ students = {
 }
 
 name = input("Enter student name : ")
-'''
+
 try:
     marks = students[name]   # try to get marks
     print(f"{name}'s marks: {marks}")
 except KeyError:
-    print(f"❌ Student '{name}' not found in the records.")
+    print(f" Student '{name}' not found in the records.")
         
-'''
-marks = students[name]   # try to get marks
-print(f"{name}'s marks: {marks}")
